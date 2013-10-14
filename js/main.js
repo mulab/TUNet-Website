3390d9c62f3f8513a061c1b12306fcd8b35e3c67
Glassices <glassices@gmail.com>
2013-10-15 01:20:46 +0800

    截图

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+function GetAbsoluteLocation(element) 
+{ 
+    if (arguments.length != 1 || element == null) 
+    { 
+        return null; 
+    } 
+    var offsetTop = element.offsetTop; 
+    while(element = element.offsetParent) 
+    { 
+        offsetTop += element.offsetTop; 
+    } 
+    return { absoluteTop: offsetTop }; 
+}
+function min(x, y)
+{
+    if (x < y)
+        return x;
+    else
+        return y;
+}
+window.onresize = function() {
+    img = document.getElementById("mainImg");
+    imgTop = GetAbsoluteLocation(img).absoluteTop;
+    widget = document.getElementById("mainWidget");
+    widgetTop = GetAbsoluteLocation(widget).absoluteTop;
+    h = min($(window).height()-widgetTop, imgTop-widgetTop+$("#mainImg").height());
+    $("#mainWidget").css("height", h+"px");
+
+}
+window.onload = function() {
+    img = document.getElementById("mainImg");
+    imgTop = GetAbsoluteLocation(img).absoluteTop;
+    widget = document.getElementById("mainWidget");
+    widgetTop = GetAbsoluteLocation(widget).absoluteTop;
+    h = min($(window).height()-widgetTop, imgTop-widgetTop+$("#mainImg").height());
+    $("#mainWidget").css("height", h+"px");
+}
 
